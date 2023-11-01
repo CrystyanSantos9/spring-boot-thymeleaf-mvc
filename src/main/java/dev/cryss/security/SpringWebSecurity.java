fd898a981508f80729b41fec8e1a15affb3ea020
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2023-11-01 17:44:30 -0300

    security with NoPassEnconder

--- src/main/java/dev/cryss/security/SpringWebSecurity.java
+++ src/main/java/dev/cryss/security/SpringWebSecurity.java
@@ -1,16 +1,13 @@
 package dev.cryss.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -21,7 +18,7 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
 		http.csrf()
 		.disable()
 		.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/")
+		.antMatchers(HttpMethod.GET, "/**")
 		.permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll()
@@ -32,9 +29,9 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception
 		{
-		    auth.inMemoryAuthentication()
+		    auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
 		    .withUser("admin")
-		    .password(passwordEnconder().encode("123456"))
+		    .password("123")
 		    .roles("ADMIN");
 		}
 	
@@ -42,10 +39,4 @@ public class SpringWebSecurity extends WebSecurityConfigurerAdapter {
 	 public void configure(WebSecurity web) throws Exception {
 		 web.ignoring().antMatchers("/materialize/**");
 	 }
-	 
-	 @Bean
-	 public PasswordEncoder passwordEnconder() {
-		 return new BCryptPasswordEncoder();
-	 }
-	 
 }
